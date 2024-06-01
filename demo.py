190a53e6e9a46baf73ea7dec7b389c75cd33693b
Tommycpg-ce6 <tommy.chuapg@gmail.com>
2024-06-01 12:36:40 +0800

    demo.py

---
+++ demo.py
@@ -0,0 +1,36 @@
+# run python3
+
+# Datatypes
+# numbers
+123
+123.12
+# string
+"hello world"
+"Hello"
+"h"
+# list
+[1,2,3,4]
+
+# dict
+{"name": "sara"}
+
+# If Else
+a = 10
+if a < 10:
+    print("This is a big number")
+else: 
+    print("This is a small number")
+
+# Loops (While)
+a = 0
+while a < 10:
+    print(f"a: {a}")
+    a = a + 1
+
+def hello(name):
+    print("Hello World", {name})
+hello("Tommy")
+
+hello("Tommy") #<--Not valid string
+hello('Tommy') #<-- Not valid string
+# hello(Tommy) # <-- invalid variable
