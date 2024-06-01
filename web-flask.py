61a8fd51f5c24a38ea5846d6fdd00498659a08cc
Tommycpg-ce6 <tommy.chuapg@gmail.com>
2024-06-01 11:27:07 +0800

    web-flask.py

--- web-flask.py
+++ web-flask.py
@@ -1,4 +1,11 @@
-# Setup
-pip3 install flask
-# Run
-python3 web-flask.py
+# Simple web application using Flask framework
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
